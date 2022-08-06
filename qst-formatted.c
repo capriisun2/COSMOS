bf4b739061e7fd4e1407606bc3589032f8908b86
Nolan Poe <npgo22@gmail.com>
2022-08-05 17:21:51 -0700

    add nolan things, delete old qst, some formatting

--- qst-formatted.c
+++ qst-formatted.c
@@ -1,149 +1,124 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "get_eigenvaluesv2.h"
 
-int main(){
-int k,i,j,N =7;
-double thop=1,E=0,t;
-FILE * fileout1;
-FILE * fileout2;
-FILE * fileout3;
-FILE * fileout4;
-FILE * fileout5;
-fileout1 = fopen("eigvals", "w");
-fileout2 = fopen("matrix", "w");
-fileout3 = fopen("eigvects", "w");
-fileout4 = fopen("psi", "w");
-fileout5 = fopen("prob", "w");
+int main() {
+  int k, i, j, N = 7;
+  double thop = 1, E = 0, t;
+  FILE* eigvalslog;
+  FILE* matrixlog;
+  FILE* eigvectslog;
+  FILE* psilog;
+  FILE* problog;
+  eigvalslog = fopen("out/eigvals", "w");    // fileout1
+  matrixlog = fopen("out/matrix", "w");      // fileout2
+  eigvectslog = fopen("out/eigvects", "w");  // fileout3
+  psilog = fopen("out/psi", "w");            // fileout4
+  problog = fopen("out/prob", "w");          // fileout5
 
-double mat[N][N];
-double newMat[N][N];
-double mat_r[N][N];
-double mat_i[N][N];
-double psi[N];
-double psi_r[N];
-double psi_i[N];
-double prob[N];
+  double mat[N][N];
+  double newMat[N][N];
+  double mat_r[N][N];
+  double mat_i[N][N];
+  double psi[N];
+  double psi_r[N];
+  double psi_i[N];
+  double prob[N];
 
-for (i = 0; i < N; i++){
-   for (j =0; j<N; j++){
-      mat[i][j]=0.0;
-      newMat[N][N] = 0.0;
-      mat_r[N][N] = 0.0;
-      mat_i[N][N] =0.0;
-   }
-}
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      mat[i][j] = 0.0;
+      newMat[N - 1][N - 1] = 0.0;
+      mat_r[N - 1][N - 1] = 0.0;
+      mat_i[N - 1][N - 1] = 0.0;
+    }
+  }
 
-for (i =0; i<N; i++){
-      psi_r[i] = 0.0;
-      psi_i[i] = 0.0;
-      psi[N] = 0.0;
-      prob[N] = 0.0;
-}
-psi_r[0] = 1.0;
-psi[0] = 1.0;
-prob[0] = 1.0;
+  for (i = 0; i < N; i++) {
+    psi_r[i] = 0.0;
+    psi_i[i] = 0.0;
+    psi[N - 1] = 0.0;
+    prob[N - 1] = 0.0;
+  }
+  psi_r[0] = 1.0;
+  psi[0] = 1.0;
+  prob[0] = 1.0;
 
-// mat[0][0] = 11;
-// mat[0][1] = 8;
-// mat[1][0] = 8;
-// mat[1][1] = -1;
-
-for (i = 0; i < N; i++){
+  for (i = 0; i < N; i++) {
     mat[i][i] = E;
-}
+  }
 
+  for (i = 1; i < N; i++) {
+    thop = sqrt(i * (N - i));
+    mat[i][i - 1] = thop;
+    mat[i - 1][i] = thop;
+  }
 
-for (i = 1; i < N; i++){
-   thop = sqrt(i * (N-i));
-   mat[i][i-1] = thop;
-   mat[i-1][i] = thop;
-}
-
-
-for(i = 0; i < N; i++)
-{
-   for(j = 0; j < N; j++)
-   {
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
       newMat[i][j] = mat[i][j];
-   }
-}
+    }
+  }
 
-printf("\n Matrix to be diagonalized:\n");
-for (i=0; i<N; i++){
-   printf("\n ");
-   for (j=0; j<N; j++){
-      fprintf(fileout2,"%8.4lf ", mat[i][j]);
-   }
-   fprintf(fileout2,"\n ");
-}
-printf("\n ");
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      fprintf(matrixlog, "%8.4lf ", mat[i][j]);
+    }
+    fprintf(matrixlog, "\n ");
+  }
 
-double val_r[N],val_i[N],vecs[N][N];
-get_eigenvalues(N, mat, val_r, val_i, vecs,1);
+  double val_r[N], val_i[N], vecs[N][N];
+  get_eigenvalues(N, mat, val_r, val_i, vecs, 1);
 
-fprintf(fileout4,"%/lf", psi[0]);
-for(t = 0; t < 4; t = t + 0.01) 
-{
-   for (int i = 0; i < N; i++){
-   for (int j =0; j<N; j++){
-      mat_r[N][N] = 0.0;
-      mat_i[N][N] =0.0;
-   } }
-   for(k = 0; k < N; k++) 
-   {
-      for(int i = 0; i < N; i++)
-      {
-         for(int j = 0; j < N; j++)
-          {
-          newMat[i][j] += ((vecs[k][i] * vecs[k][j]) * val_r[k]);
-            // double norm;
-            // double denomSum;
-            // denomSum=0;
-            // for(int h = 0; h < N; h++) 
-            //    {
-            //    denomSum += pow(vecs[k][h],2); 
-            //    }
-            // norm = 1/denomSum;
-            mat_r[i][j] += ((vecs[k][i] * vecs[k][j]) * cos(val_r[k] * t));
-            mat_i[i][j] -= ((vecs[k][i] * vecs[k][j]) * sin(val_r[k] * t));
-            psi_r[i] += ((mat_r[i][j]*psi_r[i]) - (mat_i[i][j]*psi_i[i]));
-            psi_i[i] += ((mat_r[i][j]*psi_i[i]) + (mat_i[i][j]*psi_r[i]));
-            psi[i] = psi_r[i] + psi_i[i];
-            fprintf(fileout4,"%12.8lf", psi[i]);
-            prob[i] = (psi_r[i]  * psi_r[i]) + (psi_i[i] * psi_i[i]);
-            fprintf(fileout5,"%12.8lf", prob[i]);
-          }
+  fprintf(psilog, "%lf", psi[0]);
+  for (t = 0; t < 4; t = t + 0.01) {
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+        mat_r[N - 1][N - 1] = 0.0;
+        mat_i[N - 1][N - 1] = 0.0;
       }
-   }
-}
+    }
+    for (k = 0; k < N; k++) {
+      for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+          newMat[i][j] += ((vecs[k][i] * vecs[k][j]) * val_r[k]);
+          // double norm;
+          // double denomSum;
+          // denomSum=0;
+          // for(int h = 0; h < N; h++)
+          //    {
+          //    denomSum += pow(vecs[k][h],2);
+          //    }
+          // norm = 1/denomSum;
+          mat_r[i][j] += ((vecs[k][i] * vecs[k][j]) * cos(val_r[k] * t));
+          mat_i[i][j] -= ((vecs[k][i] * vecs[k][j]) * sin(val_r[k] * t));
+          psi_r[i] += ((mat_r[i][j] * psi_r[i]) - (mat_i[i][j] * psi_i[i]));
+          psi_i[i] += ((mat_r[i][j] * psi_i[i]) + (mat_i[i][j] * psi_r[i]));
+          psi[i] = psi_r[i] + psi_i[i];
+          fprintf(psilog, "%12.8lf", psi[i]);
+          prob[i] = (psi_r[i] * psi_r[i]) + (psi_i[i] * psi_i[i]);
+          fprintf(problog, "%12.8lf", prob[i]);
+        }
+      }
+    }
+  }
 
+  for (i = 0; i < N; i++) {
+    fprintf(eigvalslog, "%12.6lf", val_r[i]);
+  }
 
+  for (j = 0; j < N; j++) {
+    for (i = 0; i < N; i++) {
+      fprintf(eigvectslog, "%8.4lf", vecs[i][j]);
+    }
 
-
-
-
-
-printf("\nEigenvalues: \n");
-for (i = 0; i < N;i++){
-fprintf(fileout1,"%12.6lf",val_r[i]);
-}
-
-printf("\nEigenVectors: \n");
-for (j = 0; j < N;j++){
-   for (i = 0; i < N; i++){
-      
-      fprintf(fileout3, "%8.4lf",vecs[i][j]);
-     
-   }
-      
-   fprintf(fileout3,"\n");
-   }
-fclose(fileout1);
-fclose(fileout2);
-fclose(fileout3);
-fclose(fileout4);
-fclose(fileout5);
-return 0;
+    fprintf(eigvectslog, "\n");
+  }
+  fclose(eigvalslog);
+  fclose(matrixlog);
+  fclose(eigvectslog);
+  fclose(psilog);
+  fclose(problog);
+  return 0;
 }
